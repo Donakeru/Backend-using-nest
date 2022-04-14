2866596786e8ba4bf3207d47102fe50938f48a7f
Daniel Alejandro Chavez Bustos <dachavezbustos@gmail.com>
2022-04-14 03:40:29 -0500

    Adicion de la jwt al servicio de Login

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -3,9 +3,18 @@ import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema, User } from 'src/schemas/user.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: 'claveSecreta?',
+      signOptions: {
+        expiresIn: '5m'
+      }
+    })
+  ],
   controllers: [AuthenticationController],
   providers: [AuthenticationService]
 })
--- src/modules/authentication/authentication.service.ts
+++ src/modules/authentication/authentication.service.ts
@@ -4,10 +4,13 @@ import { Model } from 'mongoose';
 import { UserDocument, User } from "src/schemas/user.schema";
 import { InjectModel } from '@nestjs/mongoose';
 import { LoginAuthenticationDto } from './dto/login-authentication.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthenticationService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtService: JwtService) {}
 
   async registrarUsuario(userObject: RegisterAuthenticationDto) {
     const { password } = userObject;
@@ -17,14 +20,23 @@ export class AuthenticationService {
   }
 
   async loggearUsuario(userObject: LoginAuthenticationDto) {
-    const { name, password } = userObject;
+    const { name, password} = userObject;
     const findName = await this.userModel.findOne({name: name});
-    //PAra generar errores http customizados hacemos uso del throw new seguido de la funcion Http Exception, indicaremos el mensaje de error y el codigo httpReq error
+
+    //Para generar errores http customizados hacemos uso del throw new seguido de la funcion Http Exception, indicaremos el mensaje de error y el codigo httpReq error
     if (!findName) throw new HttpException('El usuario no fué encontrado', 404);
     const validarContrasena = password == findName.password
     if (!validarContrasena) throw new HttpException('La contraseña ingresada no es correcta', 403);
 
-    return findName
+    //Para hace uso de los JWT es importante que para la encriptación no se usen datos sensibles del usuario. Lo usual es que se encripte el username + id de usuario en la BD
+    const payload = { name: findName.name, id: findName._id }
+    
+    const data = {
+      userData: findName,
+      token: this.jwtService.sign(payload),
+    }
+
+    return data
   }
 
 }
